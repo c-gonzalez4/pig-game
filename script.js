a810ded2335c0f9dce0f99f1edcf7fa8de25ca3f
Carolina Gonzalez <rpcgonzalez@gmail.com>
2020-12-17 16:30:29 -0500

    Adding the hidden class and initiating the scores to zero

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//selecting the elements
+let scorePlayer1 = document.querySelector('#score--0');
+let scorePlayer2 = document.querySelector('#score--1');
+const dice = document.querySelector('.dice');
+
+scorePlayer1.textContent = 0;
+scorePlayer2.textContent = 0;
+
+dice.classList.add('hidden');
